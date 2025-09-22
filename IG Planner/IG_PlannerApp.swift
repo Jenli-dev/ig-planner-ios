e741c9330e84de16dd3dddce24cb2de1b2d71582
Artem Zakharychev <aso.jenli@gmail.com>
2025-09-22 15:21:07 +0200

    init: IG Planner iOS project with SwiftUI

---
+++ IG Planner/IG_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IG_PlannerApp.swift
+//  IG Planner
+//
+//  Created by Artem Zakharychev on 22.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct IG_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IG PlannerTests/IG_PlannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  IG_PlannerTests.swift
+//  IG PlannerTests
+//
+//  Created by Artem Zakharychev on 22.09.2025.
+//
+
+import Testing
+
+struct IG_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
